17951de0e166bf1a6551c839e606159a9212b813
josh <joshua@marinacci.org>
2021-01-04 16:37:38 -0800

    reorganize code

--- src/App.js
+++ src/App.js
@@ -1,60 +1,17 @@
 import {useState} from 'react'
 import './App.css'
 
-import {CanvasResult, EXAMPLES, NCircle, NColor, NGradient, NList, NScalar, NString, SCOPE} from "./test1.js"
+import {EXAMPLES, SCOPE} from "./test1.js"
 import {HBox, VBox} from './ui.js'
 import {ResultArea} from './views.js'
+import {real_eval} from './lang.js'
 
-
-function real_eval(code) {
-    let scope = {
-        foo: () => console.log("doing foo"),
-        scalar: (v) => new NScalar(v),
-        string: v => new NString(v),
-        list: v => new NList(v),
-        color: v => new NColor(v),
-        gradient: v => new NGradient(v),
-        size: SCOPE.size.impl,
-        sum: SCOPE.sum.impl,
-        average: SCOPE.average.impl,
-        map: SCOPE.map.impl,
-        circle: v => new NCircle(v),
-        pack_row:SCOPE.pack_row.impl,
-        draw: SCOPE.draw.impl,
-    }
-
-    let lines = code.split("\n")
-    lines[lines.length-1] = 'return ' + lines[lines.length-1]
-
-    let defines = Object.keys(scope).map(key => {
-        return `    const ${key} = scope.${key}`
-    }).join("\n")
-    let gen_code = `
-"use strict"; 
-return function(scope) {
-${defines}
-${lines.join("\n")}
-};
-`
-    console.log("generated code is", gen_code)
-    try {
-        return Function(gen_code)()(scope)
-    } catch (e) {
-        console.error(e)
-        return e
-    }
-}
 
 function App() {
     const [code, setCode] = useState('5+6')
     const [result, setResult] = useState(null)
 
-    const doEval = (code) => {
-        // console.log("evaluating", code)
-        let res = real_eval(code)
-        // console.log("result is", res)
-        setResult(res)
-    }
+    const doEval = (code) => setResult(real_eval(code))
 
     return (
         <HBox fill>
--- src/lang.js
+++ src/lang.js
@@ -1,4 +1,4 @@
-import {CanvasResult, NColor, NGradient, NList, NScalar, NString} from './test1.js'
+import {CanvasResult, NCircle, NColor, NGradient, NList, NScalar, NString, SCOPE} from './test1.js'
 
 export const is_error_result = (result) => result instanceof Error
 export const is_scalar = (val) => (val instanceof NScalar)
@@ -8,4 +8,43 @@ export const is_color  = (val) => val instanceof NColor
 export const is_canvas_result = (val) => val instanceof CanvasResult
 export function is_gradient(result) {
     return result instanceof NGradient
+}
+
+export function real_eval(code) {
+    let scope = {
+        foo: () => console.log("doing foo"),
+        scalar: (v) => new NScalar(v),
+        string: v => new NString(v),
+        list: v => new NList(v),
+        color: v => new NColor(v),
+        gradient: v => new NGradient(v),
+        size: SCOPE.size.impl,
+        sum: SCOPE.sum.impl,
+        average: SCOPE.average.impl,
+        map: SCOPE.map.impl,
+        circle: v => new NCircle(v),
+        pack_row: SCOPE.pack_row.impl,
+        draw: SCOPE.draw.impl
+    }
+
+    let lines = code.split("\n")
+    lines[lines.length - 1] = 'return ' + lines[lines.length - 1]
+
+    let defines = Object.keys(scope).map(key => {
+        return `    const ${key} = scope.${key}`
+    }).join("\n")
+    let gen_code = `
+"use strict"; 
+return function(scope) {
+${defines}
+${lines.join("\n")}
+};
+`
+    console.log("generated code is", gen_code)
+    try {
+        return Function(gen_code)()(scope)
+    } catch (e) {
+        console.error(e)
+        return e
+    }
 }
