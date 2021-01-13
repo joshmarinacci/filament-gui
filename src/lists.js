2d21b37911c3bb16edc00f0da16e94fbce5b29d8
Josh Marinacci <jmarinacci@lyft.com>
2021-01-13 13:54:52 -0800

    move lists code to lists.js add select test and impl

---
+++ src/lists.js
@@ -0,0 +1,92 @@
+function gen_range(min,max,step) {
+    console.log("generating",min,max,step)
+    let list = []
+    for(let i=min; i<max; i+=step) {
+        list.push(i)
+    }
+    return list
+}
+
+// * __range__: generate a list of numbers: `(max), (min,max), (min,max,step)`
+
+export function range(a,b,step) {
+    if(!step) step = 1
+    if(typeof a === 'undefined' || a === null) throw new Error("range requires at least one argument")
+    if(typeof b === 'undefined') {
+        return gen_range(0,a,step)
+    } else {
+        return gen_range(a,b,step)
+    }
+}
+
+
+// * __length__: returns the length of the list
+export function length(list) {
+    if(!list) throw new Error("length(list) missing list")
+    return list.length
+}
+
+
+// * __take__: take the first N elements from a list to make a new list `take([1,2,3], 2) = [1,2]`
+export function take(list,num) {
+    if(!list) throw new Error("take needs a list to take from")
+    if(typeof num === 'undefined') num = 1
+    if(num < 0) {
+        return list.slice(list.length+num,list.length)
+    }
+    return list.slice(0,num)
+}
+
+// * __drop__: return list with the number of elements removed from the start. `drop([1,2,3],1) = [2,3]`
+export function drop(list, num) {
+    if(!list) throw new Error("take needs a list to take from")
+    if(typeof num === 'undefined') num = 1
+    if(num < 0) return list.slice(0,list.length+num)
+    return list.slice(num)
+}
+
+
+// * __join__: concatentate two lists, returning a new list. is this needed?
+export function join(a,b) {
+    if(!Array.isArray(a)) a = [a]
+    return a.concat(b)
+}
+
+
+// * __map__:  convert every element in a list using a lambda function: `(list, lam)`
+export function map(list,cb) {
+    return list.map(cb)
+}
+
+
+// * __for__:  loops over every element in a list with a lambda, but returns the original list: `(list, lam)`
+
+// * __sort__: sort list returning a new list, by: property to use for sorting `sort(data by:"date")` (should we use `order` instead?)
+export function sort(list,opts) {
+    if(opts && opts.by) {
+        return list.slice().sort((a,b)=>{
+            a = a[opts.by]
+            b = b[opts.by]
+            //special case for strings
+            if(a.localeCompare) return a.localeCompare(b)
+            return a-b
+        })
+    }
+    return list.slice().sort()
+}
+// * __reverse__: return a list with the order reversed  `reverse(data)`
+export function reverse(list) {
+    return list.reverse()
+}
+
+
+// * __sum__: adds all data points together
+export function sum(list) {
+    return list.reduce((a,b)=>a+b)
+}
+
+
+export function select(list, opts) {
+    let where = opts.where
+    return list.filter(where)
+}
--- src/lists.test.js
+++ src/lists.test.js
@@ -4,26 +4,8 @@
 
 
 import {add} from './math.js'
+import {drop, length, join, map, range, reverse, select, sort, sum, take} from "./lists.js"
 
-
-function gen_range(min,max,step) {
-    console.log("generating",min,max,step)
-    let list = []
-    for(let i=min; i<max; i+=step) {
-        list.push(i)
-    }
-    return list
-}
-function range(a,b,step) {
-    if(!step) step = 1
-    if(typeof a === 'undefined' || a === null) throw new Error("range requires at least one argument")
-    if(typeof b === 'undefined') {
-        return gen_range(0,a,step)
-    } else {
-        return gen_range(a,b,step)
-    }
-}
-// * __range__: generate a list of numbers: `(max), (min,max), (min,max,step)`
 test('make list with range', () => {
     expect(range(3)).toEqual([0,1,2])
     expect(range(10)).toEqual([0,1,2,3,4,5,6,7,8,9])
@@ -32,50 +14,25 @@ test('make list with range', () => {
     expect(range(3,8,3)).toEqual([3,6])
     expect(range(-10,0,3)).toEqual([-10,-7,-4,-1])
 })
-// * __length__: returns the length of the list
-function length(list) {
-    if(!list) throw new Error("length(list) missing list")
-    return list.length
-}
+
 test('list length', ()=>{
     expect(length([1,2,3])).toEqual(3)
     expect(length(range(10))).toEqual(10)
     expect(length(range(-5,0))).toEqual(5)
 })
 
-function take(list,num) {
-    if(!list) throw new Error("take needs a list to take from")
-    if(typeof num === 'undefined') num = 1
-    if(num < 0) {
-        return list.slice(list.length+num,list.length)
-    }
-    return list.slice(0,num)
-}
-// * __take__: take the first N elements from a list to make a new list `take([1,2,3], 2) = [1,2]`
 test("take from list",()=>{
     expect(take([1,2,3],2)).toEqual([1,2])
     expect(take([1,2,3],4)).toEqual([1,2,3])
     expect(take([1,2,3],-1)).toEqual([3])
 })
 
-function drop(list, num) {
-    if(!list) throw new Error("take needs a list to take from")
-    if(typeof num === 'undefined') num = 1
-    if(num < 0) return list.slice(0,list.length+num)
-    return list.slice(num)
-}
-// * __drop__: return list with the number of elements removed from the start. `drop([1,2,3],1) = [2,3]`
 test('drop from list',() => {
     expect(drop([1,2,3],1)).toEqual([2,3])
     expect(drop([1,2,3],-1)).toEqual([1,2])
     expect(drop([1,2,3],4)).toEqual([])
 })
 
-function join(a,b) {
-    if(!Array.isArray(a)) a = [a]
-    return a.concat(b)
-}
-// * __join__: concatentate two lists, returning a new list. is this needed?
 test('join lists',() => {
     expect(join([1,2,3],[4,5,6])).toEqual([1,2,3,4,5,6])
     expect(join([1],[4,5,6])).toEqual([1,4,5,6])
@@ -84,34 +41,11 @@ test('join lists',() => {
     expect(join([1,2,3],4)).toEqual([1,2,3,4])
 })
 
-function map(list,cb) {
-    return list.map(cb)
-}
-// * __map__:  convert every element in a list using a lambda function: `(list, lam)`
 test('map a list',()=>{
     expect(map([1,2,3],x=>x*2)).toEqual([2,4,6])
     expect(map(range(3),x=>x*2)).toEqual([0,2,4])
     expect(map(range(3), (x)=>add(x,5))).toEqual([5,6,7])
 })
-// * __for__:  loops over every element in a list with a lambda, but returns the original list: `(list, lam)`
-
-// * __sort__: sort list returning a new list, by: property to use for sorting `sort(data by:"date")` (should we use `order` instead?)
-function sort(list,opts) {
-    if(opts && opts.by) {
-        return list.slice().sort((a,b)=>{
-            a = a[opts.by]
-            b = b[opts.by]
-            //special case for strings
-            if(a.localeCompare) return a.localeCompare(b)
-            return a-b
-        })
-    }
-    return list.slice().sort()
-}
-// * __reverse__: return a list with the order reversed  `reverse(data)`
-function reverse(list) {
-    return list.reverse()
-}
 test('sorting a list',()=>{
     expect(sort([3,1,2])).toEqual([1,2,3])
     expect(reverse(sort([3,1,2]))).toEqual([3,2,1])
@@ -127,10 +61,9 @@ test('pick randomly from a list',()=>{
 })
 
 // * __select__: select items from list using where: lambda function returning false. `select(data, where=(t)=>t.amount>0)`
-// * __sum__: adds all data points together
-function sum(list) {
-    return list.reduce((a,b)=>a+b)
-}
+test('select',() => {
+    expect(select([1,2,3], {where:(x)=>x>1})).toEqual([2,3])
+})
 test('sum elements',() => {
     expect(sum([1,2,3])).toEqual(6)
     expect(sum(range(5))).toEqual(0+1+2+3+4)
