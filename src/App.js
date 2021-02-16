d20e0096118d04566ad3f247316502050202baee
josh <joshua@marinacci.org>
2021-02-16 15:03:10 -0800

    update gui to have a doc object

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import {useState} from 'react'
+import {useEffect, useRef, useState} from 'react'
 import './App.css'
 
 import {HBox, VBox} from './gui/ui.js'
 import {ResultArea} from './gui/views.js'
 import {eval_code, setup_parser} from 'filament-lang'
 
-import "codemirror/addon/hint/show-hint.css"
 import {EXAMPLES} from './gui/examples.js'
 import {CodeEditor} from './gui/editor.js'
 import {default as grammar_url} from "filament-lang/src/filament.ohm"
+
+import "codemirror/addon/hint/show-hint.css"
+import "codemirror/lib/codemirror.css"
+import * as codemirror from 'codemirror'
 
 
 let stash = ""
@@ -27,52 +30,113 @@ const CollapsablePanel = ({children, direction}) => {
     </VBox>
 }
 
-function App() {
-    const [code, setCode] = useState('5+6')
-    const [result, setResult] = useState(null)
+const realdoc = [
+    {
+        type:'filament',
+        input:"4+5",
+        output:null,
+    },
+    {
+        type:'filament',
+        input:"range(10)",
+        output:null,
+    }
+]
 
-    const doEval = async (code) => {
+
+function IOView({entry, onChange}) {
+    const ref = useRef()
+    const [editor, setEditor] = useState(null)
+    const [result, setResult] = useState(null)
+    const onEval = async (code) => {
         try {
             let grammar = await fetch(grammar_url).then(r => r.text())
-            console.log("got the grammar", grammar)
+            // console.log("got the grammar", grammar)
             await setup_parser(grammar)
             code = "{" + code + "}"
             let d = await eval_code(code)
             console.log("result is ", d)
             setResult(d)
-            console.log("done")
+            // console.log("done")
         } catch (e) {
             console.log("an error happened",e)
             setResult(e)
         }
-            // .catch(e => {
-            //     console.error("ERROR", e)
-            //     setResult(e)
-            // })
     }
+    useEffect(() => {
+        console.log("entry changed",entry)
+        if (ref.current && editor === null) {
+            console.log("making",ref.current)
+            let ed = codemirror.fromTextArea(ref.current, {
+                value: 'some cool text',
+                lineNumbers: true,
+                viewportMargin: Infinity,
+                mode: 'javascript',
+                // mode:'filament',
+                // mode: 'simplemode',
+                // hintOptions: {hint: synonyms, completeSingle: false},
+                // lineWrapping: true,
+                theme: 'elegant',
+                matchBrackets: true,
+                autoCloseBrackets: true,
+                extraKeys: {
+                    'Ctrl-Enter': () => onEval(ed.getValue()),
+                //     "Ctrl-Space": "autocomplete"
+                }
+            })
+            setEditor(ed)
+            // console.log(entry.input)
+            ed.setValue(entry.input)
+            ed.on('changes', () => onChange(ed.getValue()))
+        }
+        if(ref.current && editor !== null) {
+            editor.setValue(entry.input)
+            setResult(entry.output)
+        }
+    },[entry])
 
-    return (
-        <HBox fill>
-            <CollapsablePanel direction={'left'}>
-                <h3>Examples</h3>
-                {EXAMPLES.map(ex => <button
-                    onClick={() => setCode(ex.code.trim().split("\n")
-                        // .map(t => t.trim())
+
+    return <article>
+        <h3>block</h3>
+        <textarea ref={ref}/>
+        <button onClick={() => onEval(editor.getValue())}>eval</button>
+        <ResultArea result={result}/>
+    </article>
+}
+
+function update_doc(doc, entry, code) {
+    console.log("updating",doc,entry,code)
+}
+
+function App() {
+    const [doc, setDoc] = useState(realdoc)
+    let entries = doc.map((entry,i) => <IOView key={i} entry={entry} onChange={(code)=>update_doc(doc,entry,code)}/>)
+    let style = {
+        display:"flex",
+        flexDirection:'row',
+        border:'1px solid blue',
+    }
+    return <div style={style}>
+        <CollapsablePanel direction={'left'}>
+            <h3>Examples</h3>
+            {EXAMPLES.map(ex => <button
+                onClick={() => setDoc([{
+                    type:'filament',
+                    input:ex.code.trim().split("\n")
                         .filter(t => t.length > 0)
-                        .join("\n"))}
-                >{ex.title}</button>)}
-            </CollapsablePanel>
-            <VBox grow>
-                <CodeEditor value={code} onEval={(code)=>doEval(code)} onChange={str => stash=str}/>
-                <button onClick={() => doEval(stash)}>eval</button>
-                <ResultArea result={result}/>
-            </VBox>
-            <CollapsablePanel direction={'right'}>
-                <h3>Docs</h3>
-                {/*{Object.values(SCOPE).map(fn => <div><b>{fn.title}</b>:<i>{fn.doc}</i></div>)}*/}
-            </CollapsablePanel>
-        </HBox>
-    )
+                        .join("\n"),
+                    output:null
+                }])}
+            >{ex.title}</button>)}
+        </CollapsablePanel>
+        <VBox grow style={{
+            flex:'1.0',
+            border:'1px solid green',
+        }}>{entries}</VBox>
+        <CollapsablePanel direction={'right'}>
+            <h3>Docs</h3>
+        </CollapsablePanel>
+    </div>
 }
 
 export default App
--- src/gui/ui.js
+++ src/gui/ui.js
@@ -1,25 +1,27 @@
 import {useEffect, useRef, useState} from 'react'
 
-export const VBox = ({children, classes={}, grow = false}) => {
+export const VBox = ({children, classes={}, grow = false, fill=false}) => {
     let style = {
         display: 'flex',
         flexDirection: 'column'
     }
     let clsses = {
-        grow: grow
+        grow: grow,
+        fill:fill,
     }
     Object.keys(classes).forEach(key => clsses[key] = classes[key])
-    let clssstr = Object.keys(clsses).filter(k => clsses[k]).join(" ")
+    let clssstr = "vbox " + Object.keys(clsses).filter(k => clsses[k]).join(" ")
     return <div className={clssstr} style={style}>{children}</div>
 }
-export const HBox = ({children, fill = false}) => {
+export const HBox = ({children, grow=false, fill = false}) => {
     let style = {
         display: 'flex',
         flexDirection: 'row'
     }
     let clsses = {
-        fill: fill
+        fill: fill,
+        grow: grow,
     }
-    let clssstr = Object.keys(clsses).filter(k => clsses[k]).join(" ")
+    let clssstr = "hbox " + Object.keys(clsses).filter(k => clsses[k]).join(" ")
     return <div className={clssstr} style={style}>{children}</div>
 }
