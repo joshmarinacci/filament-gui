0e6bf18888f43e63f83b137a138aadefef9a71e9
josh <joshua@marinacci.org>
2021-02-22 13:46:23 -0800

    show short summary if available refactor symbols panel a bit

--- src/SymbolsPanel.js
+++ src/SymbolsPanel.js
@@ -1,3 +1,22 @@
+function Constant({symbol}) {
+    return <li><i>const</i> <b>{symbol.name}</b></li>
+}
+
+function FunctionSymbol({symbol}) {
+    let parms = Object.entries(symbol.params).map(([name, val]) => {
+        return <li>{name}</li>
+    })
+    return <li>
+        <i>fun</i>&nbsp;
+        <b>{symbol.name}(</b>
+        <ul className={'parameters'}>{parms}</ul>
+        <b>)</b>
+        <span className={'summary'}>
+            {symbol.summary}
+        </span>
+    </li>
+}
+
 export function SymbolsPanel({scope}) {
     console.log(scope)
     let names = scope.names().slice()
@@ -5,20 +24,9 @@ export function SymbolsPanel({scope}) {
     return <ul className={'symbols'}>
         {names.map((name) => {
             let fun = scope.funs[name]
-            console.log(fun)
-            if(fun.type === 'scalar') {
-                return <li key={name}><i>const</i> <b>{name}</b></li>
-            }
-            let parms = Object.entries(fun.params).map(([name, val]) => {
-                console.log(name, val)
-                return <li>{name}</li>
-            })
-            return <li key={name}>
-                <i>fun</i>&nbsp;
-                <b>{name}(</b>
-                <ul className={'parameters'}>{parms}</ul>
-                <b>)</b>
-            </li>
+            if(fun.type === 'scalar') return <Constant key={name} symbol={fun}/>
+            // console.log('symbol is',fun.type)
+            return <FunctionSymbol key={name} symbol={fun}/>
         })}
     </ul>
 }
