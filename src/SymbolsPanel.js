11cf841c81412b6132a77caa7317bcd37ff573cc
Josh Marinacci <jmarinacci@lyft.com>
2021-03-12 14:34:41 -0800

    load generated docs restyle the symbol panel

--- src/SymbolsPanel.js
+++ src/SymbolsPanel.js
@@ -1,19 +1,55 @@
+import API_RAW from "filament-lang/src/api.json.js"
+import {useState} from 'react'
+import "./symbolspanel.css"
+
+const API = JSON.parse(API_RAW)
 function Constant({symbol}) {
     return <li><i>const</i> <b>{symbol.name}</b></li>
 }
 
+function search_doc(name) {
+    for(let group of Object.values(API)) {
+        for( let doc of group) {
+            if(doc.name === name) {
+                return doc
+            }
+        }
+    }
+    return null
+}
+
 function FunctionSymbol({symbol}) {
-    let parms = Object.entries(symbol.params).map(([name, val]) => {
-        return <li>{name}</li>
-    })
-    return <li>
-        <i>fun</i>&nbsp;
-        <b>{symbol.name}(</b>
-        <ul className={'parameters'}>{parms}</ul>
-        <b>)</b>
-        <span className={'summary'}>
-            {symbol.summary}
-        </span>
+    let [open, setOpen] = useState(false)
+
+    let rendered_doc = ""
+    let doc = search_doc(symbol.name)
+    if(doc) {
+        console.log("matched a doc",doc)
+        let parms = Object.entries(doc.params).map(([name, val]) => {
+            return <li>{name}:{val}</li>
+        })
+        rendered_doc = <div className={'accordion-content'}>
+            <p className={'summary'}><b>function</b> {doc.summary}</p>
+            <ul className={'parameters'}>{parms}</ul>
+            {doc.examples.map(ex => {
+                return <blockquote className={'example'}>{ex.toString().trim()}</blockquote>
+            })}
+        </div>
+    } else {
+        let parms = Object.entries(symbol.params).map(([name, val]) => {
+            return <li>{name}</li>
+        })
+        rendered_doc = <div className={'accordion-content'}>
+            <i>fun</i>&nbsp;
+            <b>{symbol.name}(</b>
+            <p className={'summary'}>{symbol.summary}</p>
+            <ul className={'parameters'}>{parms}</ul>
+            <b>)</b>
+        </div>
+    }
+    return <li className={'accordion-item ' + (open?"opened":"closed")}>
+        <h3 className={'accordion-button'} onClick={()=>setOpen(!open)}>{symbol.name}</h3>
+        {rendered_doc}
     </li>
 }
 
